6f3579003abadc092c3dae17d273f04ee633c587
fangvv <fangvv@gmail.com>
2023-10-03 10:38:18 +0800

    wanshanziwo

---
+++ .github/workflows/search.py
@@ -0,0 +1,13 @@
+import requests
+
+url = 'https://wanshanziwo.eu.org/'
+response = requests.get(url) 
+html = response.text
+
+lines = html.split('\n')
+search_str = 'https://wanshanziwo.eu.org/airport/'
+
+for line in lines:
+    if search_str in line:
+        print(line)
+        break
